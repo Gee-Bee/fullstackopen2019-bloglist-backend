97a87adfb0f1d32d13e81552bf3ae877b2c594aa
Grzegorz Bunia <g.bunia@american-systems.pl>
2019-09-27 20:50:27 +0200

    4.2 Blog list, step2
    extract blogs controller

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const Blog = require('./models/blog');
+const blogRouter = require('./controllers/blogs');
 
 const mongoUrl = config.MONGODB_URI;
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -12,22 +12,6 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 app.use(cors())
 app.use(bodyParser.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogRouter);
 
 module.exports = app;
