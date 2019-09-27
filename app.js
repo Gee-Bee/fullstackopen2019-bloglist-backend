db436b6340ad9458a42881bd0459a49ffd25d8e5
Grzegorz Bunia <g.bunia@american-systems.pl>
2019-09-27 20:01:42 +0200

    extract Blog model

--- app.js
+++ app.js
@@ -1,18 +1,10 @@
 const config = require('./utils/config');
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const mongoose = require('mongoose')
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 const mongoUrl = config.MONGODB_URI;
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
